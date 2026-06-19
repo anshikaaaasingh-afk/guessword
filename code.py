3743fe0cb1c2a942ebd8713a6eeda368a26710d8
anshika <anshikaaaasingh@gmail.com>
2026-06-19 11:21:06 +0530

    initial commit

---
+++ code.py
@@ -0,0 +1,21 @@
+import random
+word_bank = ['world', 'gorgeous', 'skibidi', 'kindness','reel','rizz']
+word = random.choice(word_bank)
+guessedWord=['_']*len(word)
+attempts=10
+while attempts>0:
+    print('\n Current word: '+ ' '.join(guessedWord))
+    guess = input('Guess a letter: ').lower()
+    if guess in word:
+        for i in range(len(word)):
+            if word[i]==guess:
+                guessedWord[i]=guess
+        print('great guess!')
+    else:
+        attempts -= 1
+        print('Wrong guess! Attempts left: '+ str(attempts))
+    if '_' not in guessedWord:
+        print('\nCongratulations!! You guessed the word : ' + word)
+        break
+if attempts == 0 and '_' in guessedWord:
+    print('\nYou\'ve run out of attempts! The word was: ' + word)
